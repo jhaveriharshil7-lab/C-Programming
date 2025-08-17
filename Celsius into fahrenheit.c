626a38659d12aa867f740b6eacb57e08a77167e4
jhaveriharshil7-lab <jhaveriharshil7@gmail.com>
2025-08-17 04:12:44 -0700

    Add files via upload

---
+++ Celsius into fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   float fahrenheit,celsius ;
+    printf("Enter celsius ");
+    scanf("%f",&celsius);
+    fahrenheit=9/5*celsius+32;
+    printf("%f fahrenheit",fahrenheit);
+    getch();
+}
