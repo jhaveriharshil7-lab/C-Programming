430ae4609b2bfd209ab20146a10260bb7f1bbb42
jhaveriharshil7-lab <jhaveriharshil7@gmail.com>
2025-08-17 03:44:53 -0700

    Add files via upload

---
+++ Dollars into rupees.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   int dollars,rupees ;
+    printf("Enter dollars");
+    scanf("%d",&dollars);
+    rupees=dollars*48;
+    printf("%d rupees",rupees);
+    getch();
+}
