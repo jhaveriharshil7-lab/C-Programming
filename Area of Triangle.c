307147f3103455d211521f7dd584041337b6a153
jhaveriharshil7-lab <jhaveriharshil7@gmail.com>
2025-08-17 04:52:26 -0700

    Add files via upload

---
+++ Area of Triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   float height,base,area ;
+    printf("Enter base");
+    scanf("%f",&base);
+    printf("Enter height");
+    scanf("%f",&height);
+    area=(base*height)/2;
+    printf("%f area",area);
+    getch();
+}
