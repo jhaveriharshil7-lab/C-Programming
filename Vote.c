61ab9cbc46acc48f1f10dd53e607f9f3b064c246
jhaveriharshil7-lab <jhaveriharshil7@gmail.com>
2025-08-19 10:41:51 +0530

    Add files via upload

---
+++ Vote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   int age;
+    printf("Enter Age ");
+    scanf("%d",&age);
+    if(age>=18)
+    {printf("Eligible to Vote");}
+    else
+    {printf("Not eligible to Vote");}
+    getch();
+}
